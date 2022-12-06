f6a72cc8c96fc0e18fa554562c888ac256f96501
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-12-07 00:07:00 +0500

    Add final push

---
+++ js/masala.js
@@ -0,0 +1,49 @@
+//* 1-exercise
+// function addition(a, b) {
+//   return a + b;
+// }
+
+//* 2-exercise
+// function convert(minutes) {
+//   return minutes * 60;
+// }
+
+//* 3-exercise
+// function addition(num) {
+//   return num + 1;
+// }
+
+//* 4-exercise
+// function triArea(base, height) {
+//   return (base * height) / 2;
+// }
+
+//* 5-exercise
+// function remainder(x, y) {
+//   return x % y;
+// }
+
+//* 6-exercise
+// function calcAge(age) {
+//     return age * 365;
+// }
+
+//* 7-exercise
+// function footballPoints(wins, draws, losses) {
+//   return wins * 3 + draws * 1;
+// }
+
+//* 8-exercise
+// function points(twoPointers, threePointers) {
+// 	return twoPointers * 2 + threePointers * 3;
+// }
+
+//* 9-exercise
+// function lessThan100(a, b) {
+//   return a + b < 100;
+// }
+
+//* 10-exercise
+// function animals(chickens, cows, pigs) {
+//   return chickens * 2 + cows * 4 + pigs * 4;
+// }
